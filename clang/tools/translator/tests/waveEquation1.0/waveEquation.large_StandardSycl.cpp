d58a89f2969f58f6b21630ed6775089a2efce092
lusj <2956102778@qq.com>
2025-09-22 12:01:33 +0800

    debug

--- clang/tools/translator/tests/waveEquation1.0/waveEquation.large_StandardSycl.cpp
+++ clang/tools/translator/tests/waveEquation1.0/waveEquation.large_StandardSycl.cpp
@@ -12,11 +12,14 @@ const int NY = 128;    // y方向网格数量
 const double Lx = 10.0f; // x方向长度
 const double Ly = 10.0f; // y方向长度
 const double c = 1.0f;   // 波速
-const int TIME_STEPS = 100; // 时间步数
+const int TIME_STEPS = 1000; // 时间步数
+
 
 int main() {
     // 网格步长
     double dx = Lx / (NX - 1);
+
+
     double dy = Ly / (NY - 1);
     
     // CFL条件
